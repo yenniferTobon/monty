851603e169b8e37910c2e346b40be945ca7ce225
yenniferTobon <ymtobony@unal.edu.co>
2019-09-26 18:47:00 +0000

    fun_operators.c operation

---
+++ fun_operators.c
@@ -0,0 +1,86 @@
+#include "monty.h"
+
+/**
+* fun_operators - constructor function
+* @operation: operation
+* @top: pointer to struct
+* @Nlinea: number of line by error
+*
+* Return: 0
+*/
+int fun_operators(char *operation, stack_t **top, int Nlinea)
+{
+	int i = 0, flag = 0;
+
+	instruction_t f_ope[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{NULL, NULL},
+	};
+	while (f_ope[i].opcode != NULL)
+	{
+		if (strcmp(operation, f_ope[i].opcode) == 0)
+		{
+			f_ope[i].f(top, Nlinea);
+			flag = 1;
+		}
+		i++;
+	}
+	if (flag != 1)
+	funErrorNoOPCODE(Nlinea, "unknown instruction", operation);
+	return (0);
+}
+
+/**
+* _pall - function that print all stack
+* @stack: stack
+* @line_number: number of line by error
+*
+* Return: 0
+*/
+void _pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+	(void)line_number;
+
+	if (*stack == NULL)
+		return;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
+
+/**
+* _push - function that add value to stack
+* @top: pointer to struct
+* @line_number: number of line by error
+*
+* Return: 0
+*/
+void _push(stack_t **top, unsigned int line_number)
+{
+	stack_t *neW = NULL;
+	(void)line_number;
+
+	neW = malloc(sizeof(stack_t));
+	if (neW == NULL)
+	{
+		funErrorGeneric("Error: malloc failed");
+	}
+	neW->n = 0;
+	neW->prev = NULL;
+	if (*top == NULL)
+	{
+		neW->next = NULL;
+		*top = neW;
+	}
+	else
+	{
+		neW->next = *top;
+		(*top)->prev = neW;
+		*top = neW;
+	}
+}
